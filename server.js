c64bfe63c6b5e9824253018cbfe1dfd5fa892bcd
Dev <cynaptique@gmail.com>
2020-01-10 11:57:50 +0100

    move catchError file. Make server.js runnable directly

--- server.js
+++ server.js
@@ -4,41 +4,37 @@ const fs = require('fs')
 const path = require('path')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
-const PiModuleHelper = require('pimodule')
+// const PiModuleHelper = require('pimodule')
 
-module.exports = async () => {
-  global.PiModuleHelper = new PiModuleHelper(process.env.piModuleAddressType)
+// global.PiModuleHelper = new PiModuleHelper(process.env.piModuleAddressType)
 
-  let webserver = express()
-  webserver.use(bodyParser.json())
-  webserver.use(bodyParser.urlencoded({ extended: true }))
-  webserver.use(express.static('public'))
+let webserver = express()
+webserver.use(bodyParser.json())
+webserver.use(bodyParser.urlencoded({ extended: true }))
+webserver.use(express.static('public'))
 
-  webserver.use(cors({
-    origin: true,
-    methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'PUT'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }))
+webserver.use(cors({
+  origin: true,
+  methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'PUT'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
-  // Checking authorisations
-  webserver.use((req, res, next) => {
-    const hashedPassword = req.headers.authorization
-    if (process.env.apiPassword && bcrypt.compareSync(process.env.apiPassword, hashedPassword)) {
-      next()
-    } else {
-      res.status(403).json({
-        message: 'No authorisations'
-      })
-    }
-  })
+// Checking authorisations
+webserver.use((req, res, next) => {
+  const hashedPassword = req.headers.authorization
+  if (process.env.apiPassword && bcrypt.compareSync(process.env.apiPassword, hashedPassword)) {
+    next()
+  } else {
+    res.status(403).json({
+      message: 'No authorisations'
+    })
+  }
+})
 
-  webserver.listen(process.env.apiPort || 7070)
+webserver.listen(process.env.apiPort || 7070)
 
-  // import all the pre-defined routes that are present in /api/controller that will create the restfull API
-  let normalizedPathRoutes = path.join(__dirname, 'controllers')
-  fs.readdirSync(normalizedPathRoutes).forEach(function (file) {
-    require('./controllers/' + file)(webserver)
-  })
-
-  return webserver
-}
+// import all the pre-defined routes that are present in /api/controller that will create the restfull API
+let normalizedPathRoutes = path.join(__dirname, 'controller')
+fs.readdirSync(normalizedPathRoutes).forEach(function (file) {
+  require('./controller/' + file)(webserver)
+})
